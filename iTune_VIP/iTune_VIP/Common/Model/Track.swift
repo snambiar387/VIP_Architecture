59e0398fe4f8b19b2a7908579cf17744363970a7
sreehari m nambiar <smnambiar@apple.com>
2018-08-01 16:09:08 +0530

    Added track model

---
+++ iTune_VIP/iTune_VIP/Common/Model/Track.swift
@@ -0,0 +1,21 @@
+//
+//  Track.swift
+//  iTune_VIP
+//
+//  Created by sreehari m nambiar on 01/08/18.
+//  Copyright © 2018 sreehari m nambiar. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    
+    let wrapperType: String
+    let kind: String
+    let artistName: String
+    let trackId: Int
+    let collectionName: String
+    let trackName: String
+    let trackViewUrl: String
+    let previewUrl: String
+}
