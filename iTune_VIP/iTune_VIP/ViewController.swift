b50838248cf86c9be234a4afc3b5047e2249e668
sreehari m nambiar <smnambiar@apple.com>
2018-08-01 15:59:09 +0530

    Created Xcode project.

---
+++ iTune_VIP/iTune_VIP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTune_VIP
+//
+//  Created by sreehari m nambiar on 01/08/18.
+//  Copyright © 2018 sreehari m nambiar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
